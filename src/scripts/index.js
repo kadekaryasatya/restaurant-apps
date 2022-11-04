c95476b590fa42746de1eb83a8823bb7d1331d1a
dekarya123 <arya.satyad123@gmail.com>
2022-11-04 17:51:24 +0800

    Add Hamburger

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,21 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
 
-console.log('Hello Coders! :)');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav__item");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
